c0583fb39ed0f1738af8053fd3abec8dfd7919a2
MathurJi0309 <anileshjssacademy84@gmail.com>
2026-02-21 15:08:28 +0530

    api ApiResonse and api error handling utilities to the project.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-import express from "express";
+import {app} from "./app.js";
 dotenv.config({path: "./.env"});
+const PORT = process.env.PORT || 5000;
 
 
 
-const app = express();
 
 connectDB().then(() => {
-    app.listen(process.env.PORT, () => {
-        console.log(`Server is running on port ${process.env.PORT}`);
+    app.on('error', (err) => {
+        console.error("Server error:", err);
     });
+    app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
 }).catch((err) => {
-    console.error("Failed to connect to MongoDB:", err);
-    process.exit(1);
+    console.error("Failed to connect to the database:", err);
 });
